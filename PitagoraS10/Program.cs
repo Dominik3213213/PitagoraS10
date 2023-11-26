3911ee8f004ba6cb74ed429438a04f7f3132ffcb
ST33-217\Student <ryszard.kuczowic@chorzow.merito.pl>
2023-11-26 15:48:47 +0100

    Dodaj pliki projektów.

---
+++ PitagoraS10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Pitagoras
+{
+	static void Main(string[] args)
+	{
+		double a, b, c;
+		Console.WriteLine("Podaj A");
+		a = double.Parse(Console.ReadLine());
+		Console.WriteLine("Podaj B");
+		b = double.Parse(Console.ReadLine());
+		c = (a * a + b * b);
+		Console.WriteLine("C wynosi : {0}", c);
+		c = Math.Sqrt(a * a + b * b);
+		Console.WriteLine("Pierwiastek z C wynosi: {0}", c);
+		Console.ReadLine();
+
+	}
+}
